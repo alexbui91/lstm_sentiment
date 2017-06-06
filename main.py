b4fe862744aebe5204f8f51900ab3499f6995d38
cuongbt <cuong.buitien@olbius.vn>
2017-06-06 18:31:00 +0900

    ask more patience time

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ word_vectors, vocabs = None, None
 #word_vector_path="../data/glove.6B.300d.txt"
 #import main; main.exe(word_vectors_file="../data/glove.6B.50d.txt", word_vectors_path="./data")
 #import main; main.exe(word_vectors_file="../data/glove_text8.txt", word_vectors_path="../cnn_sentiment/data")
-def exe(word_vectors_file, vector_preloaded_path, train_path, dev_path, test_path, hidden_sizes, maxlen, pep, fep):
+def exe(word_vectors_file, vector_preloaded_path, train_path, dev_path, test_path, hidden_sizes, maxlen, pep, fep, ppat, fpat):
     global word_vectors, vocabs
     if os.path.exists(train_path) and os.path.exists(dev_path) and os.path.exists(test_path):
         train = utils.load_file(train_path)
@@ -24,9 +24,9 @@ def exe(word_vectors_file, vector_preloaded_path, train_path, dev_path, test_pat
         word_vectors, vocabs = utils.loadWordVectors(word_vectors_file, vector_preloaded_path)
     if not maxlen:
         maxlen = properties.maxlen
-    lstm = Model(word_vectors, hidden_sizes=hidden_sizes, epochs=pep)
+    lstm = Model(word_vectors, hidden_sizes=hidden_sizes, epochs=pep, patience=ppat)
     lstm_params = lstm.train(train, dev, test, maxlen)
-    combined = LSTM_CNN(word_vectors, hidden_sizes=hidden_sizes, epochs=fep, lstm_params=lstm_params)
+    combined = LSTM_CNN(word_vectors, hidden_sizes=hidden_sizes, epochs=fep, lstm_params=lstm_params, patience=fpat)
     combined.train(train, dev, test, maxlen)
 
 #python main.py --train='../data/50d.training_twitter_full.txt' --dev='../data/50d.dev_twitter_small.txt' --test='../data/50d.test_twitter.txt' --vectors='../data/glove.6B.50d.txt' --plvec='../data'
@@ -42,7 +42,9 @@ parser.add_argument('--max', type=int, default=140)
 parser.add_argument('--hs', type=list, default=[50, 10, 2])
 parser.add_argument('--pep', type=int, default=10)
 parser.add_argument('--fep', type=int, default=20)
+parser.add_argument('--ppat', type=int, default=20)
+parser.add_argument('--fpat', type=int, default=20)
 
 args = parser.parse_args()
 
-exe(args.vectors, args.plvec, args.train, args.dev, args.test, args.hs, args.max, args.pep, args.fep)
+exe(args.vectors, args.plvec, args.train, args.dev, args.test, args.hs, args.max, args.pep, args.fep, args.ppat, args.fpat)
